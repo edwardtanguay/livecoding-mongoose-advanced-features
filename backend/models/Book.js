920783bb62584f90f84e61faece328e44d551d71
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-07 11:38:14 +0200

    author schema

---
+++ backend/models/Book.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const authorSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	url: String,
+	email: String
+});
+
+const bookSchema = new mongoose.Schema({
+	title: String,
+	description: String,
+	numberOfPages: Number,
+	language: String,
+	imageUrl: String,
+	buyUrl: String,
+	whenPurchased: Date,
+	relatedBook: mongoose.SchemaTypes.ObjectId,
+	topics: [String],
+	author: authorSchema,
+});
+
+export const Book = mongoose.model('book', bookSchema);
